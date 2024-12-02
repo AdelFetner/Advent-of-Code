a73cc1f87973d68959216e44d70ecc1edf5e2743
Adel Fetner <adelfetner@gmail.com>
2024-12-01 23:42:43 -0300

    first part of day 1 done

---
+++ 2024/1stAdvent/1stAdvent/Program.cs
@@ -0,0 +1,10 @@
+﻿var filepath = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName, "input.txt");
+var input = File.ReadAllLines(filepath);
+Console.WriteLine($"Number of lines: {input.Length}");
+Console.WriteLine($"First line: {input[0]}");
+
+var leftList = input.Select(x => x.Split("   ")[0]).Select(int.Parse).Order().ToList();
+var rightList = input.Select(x => x.Split("   ")[1]).Select(int.Parse).Order().ToList();
+
+var solution = Enumerable.Range(0, leftList.Count).Select(i => Math.Abs(leftList[i] - rightList[i])).Sum();
+Console.WriteLine(solution);
